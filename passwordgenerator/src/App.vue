7ab585f47cc2cabbe2cd50295b03af15b90b2cd2
Thomas U <ToomanyPacketsglobal@gmail.com>
2023-02-23 23:57:24 +0100

    Changes on App.vue path, components name...

--- passwordgenerator/src/App.vue
+++ passwordgenerator/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+          <passswordgenerator/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import passswordgenerator from './components/PasswortGenerator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    passswordgenerator
   }
 }
 </script>
