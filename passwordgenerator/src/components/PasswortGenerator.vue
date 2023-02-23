dd5cafd6ba36eeab5b5dbff15a9c5d25dc93affa
Thomas U <ToomanyPacketsglobal@gmail.com>
2023-02-23 23:58:20 +0100

    Add template section for title and checkboxen for password generation

---
+++ passwordgenerator/src/components/PasswortGenerator.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="passwordgenerator">
+    <h1 class="title">Passwort Generator V1</h1>
+    <div class="ui">
+      <div class="pw_input">
+        <input type="number" class="pw_length" value="16" placeholder="Passwortlänge">
+      </div>
+      <div class="checkbox_buchstaben">
+        <input type="checkbox" id="lowercase" checked value="on"><span class="text"> Kleinbuchstaben</span>
+        <input type="checkbox" id="upperrcase" checked value="on"><span class="text"> Großbuchstaben</span>
+        <input type="checkbox" id="umlauts" checked value="on"><span class="text"> Umlaute</span>
+      </div>
+
+      <div class="checkbox_zahlen">
+        <input type="checkbox" id="numbers" checked value="on"><span class="text"> Nummern</span>
+        <input type="checkbox" id="special" checked value="on"><span class="text"> Sonderzeichen</span>
+      </div>
+
+    </div>
+  </section>
+</template>
